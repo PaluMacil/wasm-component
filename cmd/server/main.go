1a6157ffba2126e08d4c8a8615a7cf46a3c193c5
Dan Wolf <dcwolf@gmail.com>
2022-06-06 23:49:04 -0400

    comments, more signal handling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 
 	"go.uber.org/zap"
@@ -24,14 +25,21 @@ func main() {
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
-		//time from when the connection is accepted to when the request body is fully read
+		// time from when the connection is accepted to when the request body is fully read
 		ReadTimeout: 5 * time.Second,
-		//time from the end of the request header read to the end of the response write
+		// time from the end of the request header read to the end of the response write
 		WriteTimeout: 10 * time.Second,
 		IdleTimeout:  120 * time.Second,
 	}
-	stop := make(chan os.Signal)
-	signal.Notify(stop, os.Interrupt, os.Kill)
+	stop := make(chan os.Signal, 1)
+	// SIGHUP  is sent when a tty disconnects, but but convention it is also commonly used to request that a daemon
+	//         reload its configuration
+	// SIGINT  is for ctrl-c in the terminal (and cannot be sent programmatically on Windows)
+	// SIGTERM is sent by Kubernetes, Docker, and others. Kubernetes gives 30 seconds to exit by default. The closest
+	//         Windows equivalent is WM_CLOSE which is sent to a windowed application when the x button is clicked, etc.
+	// SIGKILL cannot be delayed, handled, or ignored. This is how you kill a process immediately.
+	// 	       On Windows, which doesn't have signals, the equivalent is TerminateProcess (processthreadsapi.h)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		logger.Info("shutting down", zap.Error(srv.ListenAndServe()))
 	}()
@@ -42,6 +50,6 @@ func main() {
 	defer cancel()
 
 	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal(err)
+		logger.Info("shutdown failed", zap.Error(err))
 	}
 }
